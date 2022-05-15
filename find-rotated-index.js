cd15a87c9084e7c8fd91d118b80d474430245ca7
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-15 17:56:34 -0400

    write find-rotated-index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,48 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, num) {
+	let split = findStart(arr);
+	let leftIdx = 0;
+	let rightIdx = arr.length - 1;
+
+	if (arr[leftIdx] <= num && num <= arr[split - 1]) {
+		rightIdx = split - 1;
+		while (leftIdx <= rightIdx) {
+			let midIdx = Math.floor((leftIdx + rightIdx) / 2);
+			if (arr[midIdx] < num) {
+				leftIdx = midIdx + 1;
+			} else if (arr[midIdx] > num) {
+				rightIdx = midIdx - 1;
+			} else {
+				return midIdx;
+			}
+		}
+		return -1;
+	} else if (arr[split] <= num && num <= arr[rightIdx]) {
+		leftIdx = split;
+		while (leftIdx <= rightIdx) {
+			let midIdx = Math.floor((leftIdx + rightIdx) / 2);
+			if (arr[midIdx] < num) {
+				leftIdx = midIdx + 1;
+			} else if (arr[midIdx] > num) {
+				rightIdx = midIdx - 1;
+			} else {
+				return midIdx;
+			}
+		}
+		return -1;
+	}
+	return -1;
 }
 
-module.exports = findRotatedIndex
+const findStart = (arr) => {
+	let lefIdx = 0;
+	let rightIdx = arr.length - 1;
+	while (lefIdx <= rightIdx) {
+		let midIdx = Math.floor((lefIdx + rightIdx) / 2);
+		if (arr[midIdx] > arr[midIdx + 1]) {
+			return midIdx + 1;
+		}
+		arr[lefIdx] <= arr[midIdx] ? (lefIdx = midIdx + 1) : (rightIdx = midIdx - 1);
+	}
+};
+
+module.exports = findRotatedIndex;
