316942cd8ff635403f3eae4d324d3bb1e068b89a
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-15 15:58:29 -0400

    update jest and write count-zeroes

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,37 @@
-function countZeroes() {
-  
-}
+function countZeroes(arr) {
+	let leftIdx = 0;
+	let rightIdx = arr.length - 1;
+	let zeroIdx;
+	let midIdx;
 
-module.exports = countZeroes
+	while (leftIdx <= rightIdx) {
+		midIdx = Math.floor((leftIdx + rightIdx) / 2);
+		if (arr[midIdx] === 1) {
+			if (arr[midIdx + 1] === 1) {
+				leftIdx = midIdx++;
+			}
+			if (arr[midIdx + 1] === 0) {
+				zeroIdx = midIdx + 1;
+				if (zeroIdx) {
+					return arr.length - zeroIdx;
+				}
+			}
+		}
+		if (arr[midIdx] === 0) {
+			if (arr[midIdx - 1] === 0) {
+				rightIdx = midIdx--;
+			}
+			if (!arr[midIdx - 1]) {
+				return arr.length;
+			}
+			if (arr[midIdx - 1] === 1) {
+				zeroIdx = midIdx;
+				if (zeroIdx) {
+					return arr.length - zeroIdx;
+				}
+			}
+		}
+		return 0;
+	}
+}
+module.exports = countZeroes;
