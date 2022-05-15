b5e2ccf236583dca4feb72107c23ea85d57f8922
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-15 18:00:03 -0400

    write find rotation count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,14 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+	let lefIdx = 0;
+	let rightIdx = arr.length - 1;
+	while (lefIdx <= rightIdx) {
+		let midIdx = Math.floor((lefIdx + rightIdx) / 2);
+		if (arr[midIdx] > arr[midIdx + 1]) {
+			return midIdx + 1;
+		}
+		arr[lefIdx] <= arr[midIdx] ? (lefIdx = midIdx + 1) : (rightIdx = midIdx - 1);
+	}
+	return 0;
 }
-
-module.exports = findRotationCount
+console.log(findRotationCount([ 7, 9, 11, 12, 15 ]));
+module.exports = findRotationCount;
