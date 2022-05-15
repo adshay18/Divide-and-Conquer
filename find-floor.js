8ec1d73683412c230ee58120e0e795c9f7e89695
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-15 18:19:33 -0400

    write find-floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,28 @@
-function findFloor() {
-  
-}
+function findFloor(arr, num) {
+	let leftIdx = 0;
+	let rightIdx = arr.length - 1;
 
-module.exports = findFloor
+	// quick check if num is too low
+	if (num < arr[leftIdx]) {
+		return -1;
+	}
+	// quick check if num is out of range completely
+	if (num >= arr[rightIdx]) {
+		return arr[rightIdx];
+	}
+	// Otherwise search for floor
+	while (leftIdx <= rightIdx) {
+		let midIdx = Math.floor((leftIdx + rightIdx) / 2);
+
+		if (arr[midIdx] > num) {
+			rightIdx = midIdx - 1;
+		} else if (arr[midIdx] > num) {
+			leftIdx = midIdx + 1;
+		} else {
+			return arr[midIdx];
+		}
+	}
+	return -1;
+}
+console.log(findFloor([ 1, 2, 8, 10, 10, 12, 19 ], 20));
+module.exports = findFloor;
